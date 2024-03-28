070bbfc27657d488c1fe4c81d6c2c9e880bb5c65
Swetanshu <swetanshusingh17@gmail.com>
2024-03-28 14:22:33 +0530

    Add : Intital Project Files

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home(){
+  return (
+    <div className="w-full h-[100vh] p-6 flex justify-center items-center bg-black">
+
+      <section className="signup-form w-[30%] h-full flex justify-center items-center border border-gray-800 rounded-md">
+        <div className="heading text-white text-center">
+          <h2 className="text-2xl font-semibold">Create an Account</h2>
+          <p className="text-gray-400">Enter your email below to create your account</p>
+        </div>
+
+        <form action="/">
+          
+        </form>
+      </section>
+
+    </div>
+  )
+}
